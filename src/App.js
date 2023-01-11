c13a62fe93df53b0fdc3f018ec93afacffd2ac1c
Rob Horner <rdhjr81@gmail.com>
2023-01-11 15:36:44 -0500

    add toggle-able div

--- src/App.js
+++ src/App.js
@@ -4,26 +4,50 @@ import "./App.css";
 import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
 import List from "./components/List/List";
+import { Transition } from "react-transition-group/Transition";
 
 class App extends Component {
   state = {
-    modalIsOpen: false
-  }
+    modalIsOpen: false,
+    showBlock: false,
+  };
 
   showModal = () => {
-    this.setState({modalIsOpen: true});
-  }
+    this.setState({ modalIsOpen: true });
+  };
 
   closeModal = () => {
-    this.setState({modalIsOpen: false});
-  }
+    this.setState({ modalIsOpen: false });
+  };
   render() {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        {this.state.modalIsOpen && <Modal show={this.state.modalIsOpen} closed={this.closeModal}/>}
-        {this.state.modalIsOpen && <Backdrop show={this.state.modalIsOpen}/>}
-        <button className="Button" onClick={this.showModal}>Open Modal</button>
+        <button
+          className="Button"
+          onClick={() => {
+            this.setState((prevState) => ({ showBlock: !prevState.showBlock }));
+          }}
+        >
+          Toggle
+        </button>
+        <br />
+        {this.state.showBlock ? (
+          <div
+            style={{
+              backgroundColor: "red",
+              width: 100,
+              height: 100,
+              margin: "auto",
+            }}
+          ></div>
+        ) : null}
+
+        {this.state.modalIsOpen ? <Modal closed={this.closeModal} /> : null}
+        {this.state.modalIsOpen ? <Backdrop /> : null}
+        <button className="Button" onClick={this.showModal}>
+          Open Modal
+        </button>
         <h3>Animating Lists</h3>
         <List />
       </div>
